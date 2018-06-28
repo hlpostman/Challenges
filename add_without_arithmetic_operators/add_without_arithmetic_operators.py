e2035f8659886ad989439c2a65097ab72c0b9a0d
hlpostman <hlpostman@users.noreply.github.com>
2018-06-27 21:49:44 -0700

    Docstring for add_without_arithmetic_operators

---
+++ add_without_arithmetic_operators/add_without_arithmetic_operators.py
@@ -0,0 +1,15 @@
+# add_without_arithmetic_operators.py
+# 28 June 2018
+
+def add_without_arithmetic_operators(a, b):
+    """ Takes two unsigned integers and returns their sum without using arithmetic operators.
+        Args:
+            a, b: both unsigned integers
+
+        Returns:
+            an unsigned integer the sum of a and b
+
+        Raises:
+            TypeError: at least one of a and b is not an unsigned integer
+    """
+    pass
