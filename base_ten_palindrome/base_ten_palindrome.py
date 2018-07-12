3716827589171690da328a4bcd23bf667a05058e
hlpostman <users.noreply.github.com>
2018-07-11 22:11:02 -0700

    Docstring sub-parts but header needed

---
+++ base_ten_palindrome/base_ten_palindrome.py
@@ -0,0 +1,15 @@
+# base_ten_palindrome.py
+# 13 July 2018
+
+def base_ten_palindrome(n):
+    """
+
+    Args:
+        n: an integer in base ten
+
+    Returns:
+        bool: True if the digits of n form a palindrome, making n read as the same number whether read from left to right or right to left, e.g. 101; False if not.
+
+    Raises:
+        TypeError: n is not an integer.
+    """
