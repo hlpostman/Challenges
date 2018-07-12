d0ee7c002e05f9806cf1d581b4282e65abd3cc5b
hlpostman <users.noreply.github.com>
2018-07-11 21:56:29 -0700

    Outline test file

---
+++ base_ten_palindrome/test_base_ten_palindrome.py
@@ -0,0 +1,8 @@
+# test_base_ten_palindrome.py
+# 13 July 2018
+
+import pytest
+
+# Test valid input
+    # Test extreme input
+# Test bad input (bad types, bad number of arguments, bad values, etc)
