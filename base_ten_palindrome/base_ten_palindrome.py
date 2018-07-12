a584feb80dc0aafff5d199e72cb163ee602dd201
hlpostman <users.noreply.github.com>
2018-07-12 15:41:03 -0700

    Edit docstring

--- base_ten_palindrome/base_ten_palindrome.py
+++ base_ten_palindrome/base_ten_palindrome.py
@@ -2,14 +2,14 @@
 # 13 July 2018
 
 def base_ten_palindrome(n):
-    """
+    """ Detects whether or not the digits of n form a palindrome, i.e. whether n reads as the same number in base ten when read from left to right as when read from right to left, e.g. 101.
 
     Args:
         n: an integer in base ten
 
     Returns:
-        bool: True if the digits of n form a palindrome, making n read as the same number whether read from left to right or right to left, e.g. 101; False if not.
+        bool: True if the digits of n form a palindrome, False if not
 
     Raises:
-        TypeError: n is not an integer.
+        TypeError: n is not an integer
     """
