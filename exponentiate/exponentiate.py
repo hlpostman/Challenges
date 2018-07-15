6f392e9acd8e5c34c6777fb78a0dd53626182ca4
hlpostman <users.noreply.github.com>
2018-07-14 23:13:24 -0700

    Docsstring for exponentiation file

---
+++ exponentiate/exponentiate.py
@@ -0,0 +1,18 @@
+# exponentiate.py
+# 15 July 2018
+
+def exponentiate(a,b):
+    """Takes two unsigned integers a and b, and returns the value of a raised to the power of b, without using **, the Python exponentiation operator.
+
+    Args:
+        a: unsigned integer, the base for the exponentiation
+        b: unsigned integer, the exponent for the exponentiation
+
+    Returns:
+        unsigned integer, the result of raising a to the power of b
+
+    Raises:
+        TypeError: at least one of the arguments is not an integer type
+        ValueError: at least one of the arguments is signed
+    """
+    pass
