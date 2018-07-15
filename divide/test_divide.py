3b4934f7cd5c2b4d670892da613b20e961873e17
hlpostman <users.noreply.github.com>
2018-07-15 14:44:10 -0700

    Tests for valid input for division algorithm

--- divide/test_divide.py
+++ divide/test_divide.py
@@ -6,11 +6,36 @@ import divide
 
 # Test valid input
 
-# Test extreme input - 0 div anything, large numbers
+def test_basic_input():
+    # b divides a evenly
+    assert divide(10,2) == 5
+    assert divide(15,3) == 5
+    assert divide(15,5) == 3
 
-# Division by 1
-# Diviion causing 0 as output
-# Division causing floor, divsion not causing floor - two sections with comments in basic input
+    # a does NOT divide b evenly, a > b
+    assert divide(10,3) == 3
+    assert divide(15,2) == 7
+
+    # a does NOT divide b evenly, a < b
+    assert divide(10,11) == 0
+    assert divide(15,100) == 0
+
+def test_a_and_b_same_number():
+    assert divide(5,5) == 1
+    assert divide(1000,1000) == 1
+
+def test_division_by_one():
+    assert divide(1,1) == 1
+    assert divide(5,1) == 1
+    assert divide(1000,1) == 1000
+
+def test_zero_as_dividend():
+    assert divide(0,1) == 0
+    assert divide(0,2) == 0
+
+def test_large_input():
+    assert divide(9223372036854775807,1) # a is max 64-bit unsigned int (2**63 - 1)
+    assert divide(10000000000000000000,1000000000000000000) # 10**19 divided by 10**18
 
 # Test bad input
 
