e5aeec4eea9292f8c7971d4dae09fe8450d3ff5e
hlpostman <users.noreply.github.com>
2018-07-16 16:16:36 -0700

    Update tests to acommodate negative and floating point numbers in the base and negative numbers in the exponent

--- exponentiate/test_exponentiate.py
+++ exponentiate/test_exponentiate.py
@@ -16,7 +16,7 @@ def test_power_of_zero():
     assert exponentiate(1000,0) == 1
 
 def test_power_of_one():
-    assert exponentiate(2,1) == 1
+    assert exponentiate(2,1) == 2
     assert exponentiate(1000,1) == 1000
 
 def test_bases_with_invariant_output():
@@ -29,21 +29,30 @@ def test_bases_with_invariant_output():
     assert exponentiate(1,1000) == 1
 
 def test_large_input():
-    assert exponentiate(10000,1000) == 10000**1000
-    assert exponentiate(4294967296,2) == 18446744073709551616 # Result excedes max unsigned 64-bit int of 18446744073709551615 (2**63 - 1).  No overflow due to Python 3 arbitrary precision for handling of integers
+    assert exponentiate(10000,10) == 10000.0**10
+    assert exponentiate(4294967296,2) == 18446744073709551616 # Result excedes max unsigned 64-bit int of 18446744073709551615 (2**63 - 1)
+
+def test_float_base_input():
+    assert exponentiate(0.5,2) == 0.25
+    assert exponentiate(2.5,2) == 6.25
+
+def test_negative_base_input():
+    assert exponentiate(-2,2) == 4
+    assert exponentiate(-2,3) == -8
+
+def test_negative_exponent_input():
+    assert exponentiate(2,-2) == 0.25
+    assert exponentiate(-2,-2) == 0.25
+    assert exponentiate(-2,-3) == -0.125
 
 # Test bad input
 
-# Test TypeError for non
-def test_float_input():
+# Test TypeError
+def test_float_exponent_input():
     # Floored floats
     with pytest.raises(TypeError):
-        exponentiate(1.0,1)
-    with pytest.raises(TypeError):
         exponentiate(1,1.0)
-    # Floats with nonzero decimals
-    with pytest.raises(TypeError):
-        exponentiate(1.5, 1)
+    # # Floats with nonzero decimals
     with pytest.raises(TypeError):
         exponentiate(1,1.5)
 
@@ -63,7 +72,7 @@ def test_wrong_number_of_arguments():
     with pytest.raises(TypeError):
         exponentiate(1)
     # More than two arguments
-    with pytest.raises(TypeError)
+    with pytest.raises(TypeError):
         exponentiate(1,2,3)
 
 # Test ValueError
@@ -71,9 +80,5 @@ def test_zero_passed_for_both_arguments():
     # Zero to the zero is undefined
     with pytest.raises(ValueError):
         exponentiate(0,0)
-
-def test_negative_input():
     with pytest.raises(ValueError):
-        exponentiate(-1,2)
-    with pytest.raises(ValueError):
-        exponentiate(1,-2)
+        exponentiate(0.0,0.0)
