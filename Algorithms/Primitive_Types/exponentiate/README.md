bc62ec825608b6469217c58c1555c40e231b871e
hlpostman <users.noreply.github.com>
2018-07-18 18:15:58 -0700

    Reorganize files to be in folders by type within an Algorithms folder at the root 32

---
+++ Algorithms/Primitive_Types/exponentiate/README.md
@@ -0,0 +1,3 @@
+[exponentiate](https://github.com/hlpostman/challenges/blob/master/exponentiate/exponentiate.py)
+
+express the significance of being linear with respect to the msb of y rather than y.  One way to do this is to express what the time complexity actually is in terms of y - is it logarithmic?  You get one more msb in y only when y doubles.
---
+++ Algorithms/Primitive_Types/multiply_without_arithmetic_operators/multiply_without_arithmetic_operators.py
@@ -0,0 +1,47 @@
+# multiply_without_arithmetic_operators.py
+# 14 July 2018
+
+def multiply_without_arithmetic_operators(a, b):
+    """Takes two unsigned integers and returns their product without using arithmetic operators.
+
+    Args:
+        a, b: both unsigned integers
+
+    Returns:
+        an unsigned integer the product of a and b
+
+    Raises:
+        TypeError: the arguments received for a and b are not both integers
+
+    """
+    if a < 0 or b < 0:
+        raise ValueError("Both arguments must be non-negative.")
+    product = 0
+    LSB_in_b_mask = 1
+    i = 0
+    while b:
+        if b & LSB_in_b_mask != 0:
+            a_times_LSB_in_b = a << i
+            product = add_without_arithmetic_operators(product, a_times_LSB_in_b)
+            b ^= LSB_in_b_mask # Clear that place in b
+        LSB_in_b_mask <<= 1
+        i = add_without_arithmetic_operators(i, 1)
+    return product
+
+def add_without_arithmetic_operators(a, b):
+    """ Takes two unsigned integers and returns their sum without using arithmetic operators.
+
+        Args:
+            a, b: both unsigned integers
+        Returns:
+            an unsigned integer the sum of a and b
+        Raises:
+            TypeError: the arguments received for a and b are not both integers
+    """
+    if a < 0 or b < 0:
+        raise ValueError("Both arguments must be non-negative.")
+    while b != 0:
+        carry = a & b # Mask of the bits that will necessitate carrying
+        a ^= b # Mask of the bits that, at this iteration, can be added into a from b without carrying
+        b = carry << 1 # Carry all of the carry bits one place to the left
+    return a
