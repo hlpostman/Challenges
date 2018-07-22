ff73ecb36e88f71fd347cd572fe1b42039e02f46
hlpostman <users.noreply.github.com>
2018-07-22 14:10:56 -0700

    Refactor docstring to return integer rather than array

--- algorithms/collection_types/longest_consecutive_sequence/longest_consecutive_sequence.py
+++ algorithms/collection_types/longest_consecutive_sequence/longest_consecutive_sequence.py
@@ -2,11 +2,11 @@
 # 22 July 2018
 
 def longest_consecutive_sequence(arr):
-    """Takes an unsorted array of integers and returns an array of the elements that would make up the longest consecutive sequence of integers if the input array were sorted, without duplicates.  Does this in O(n) time.
+    """Takes an unsorted array of integers and returns an integer that represents the length of the longest consecutive sequence that could be made out of the elements of the input array without duplicates.  Does this in O(n) time.
 
         Args: array of integers
 
-        Returns: array of integers
+        Returns: integer
 
         Raises:
             TypeError: expected array of integers
