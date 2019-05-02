37e7fb65c87c684be8c0198733922bef9f53a10d
hlpostman <users.noreply.github.com>
2019-05-02 07:01:03 -0400

    Knuth-morris-pratt part 1 ie find repeated prefix in substring

---
+++ algorithms/primitive_types/knuth_morris_pratt/knuth_morris_pratt.py
@@ -0,0 +1,31 @@
+def knuth_morris_pratt_algorithm(string: str, substring: str) -> str:
+	substring_internal_matches = [-1 for _ in substring]
+	i, j = 1, 0
+	while i < len(substring):
+		if substring[i] == substring[j]:
+			substring_internal_matches[i] = j
+			i += 1
+			j += 1
+			continue
+		j = substring_internal_matches[j - 1] + 1 
+		if substring[i] == substring[j]:
+			continue
+		else:
+			i += 1
+
+def longest_repeated_prefix(s: str) -> str:
+	substring_internal_matches = [-1 for _ in s]
+	i, j = 1, 0
+	while i < len(s):
+		if s[i] == s[j]:
+			substring_internal_matches[i] = j
+			i += 1
+			j += 1
+			continue
+		j = substring_internal_matches[j - 1] + 1 
+		if s[i] == s[j]:
+			continue
+		else:
+			i += 1
+	start, end = 0, max(substring_internal_matches) + 1
+	return s[start:end]
