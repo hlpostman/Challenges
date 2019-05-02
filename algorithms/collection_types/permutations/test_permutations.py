c0596e507c14cf9c6d8c72e7b73c3f89b366ffcf
hlpostman <users.noreply.github.com>
2019-05-02 06:35:59 -0400

    Basic tests for permutations

--- algorithms/collection_types/permutations/test_permutations.py
+++ algorithms/collection_types/permutations/test_permutations.py
@@ -5,8 +5,12 @@ class TestPermute(unittest.TestCase):
      def test_general(self):
          input1 = [1,2,3]
          correct_output1 = [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+         input2 = [1,2,3,4]
+         correct_output2 = [[1,2,3,4], [1,2,4,3], [1,3,2,4], [1,3,4,2], [1,4,2,3], [1,4,3,2], [2,1,3,4], [2,1,4,3], [2,3,1,4], [2,3,4,1], [2,4,1,3], [2,4,3,1], [3,1,2,4], [3,1,4,2], [3,2,1,4], [3,2,4,1], [3,4,1,2], [3,4,2,1], [4,1,2,3], [4,1,3,2], [4,2,1,3], [4,2,3,1], [4,3,1,2], [4,3,2,1]]
+
          self.assertEqual(permutations.get_permutations(input1), correct_output1)
-     
+         self.assertEqual(permutations.get_permutations(input2), correct_output2)
+
      def test_non_unique(self):
          input1 = [1,1,1]
          correct_output1 = [[1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1]]
